67afa9998b2d993789916b5770ecbc30e3905f1f
Steve ALBERT <79840652+sthalbert@users.noreply.github.com>
2025-08-01 12:43:40 +0200

    Add files via upload

---
+++ ReadVms.py
@@ -0,0 +1,20 @@
+from librairies.auth_mercator import header_mercator
+from librairies.read_outscale import read_vms,read_vpcs
+from librairies.mercator import cmdb_read_vm, cmdb_create_vms, cmdb_update_vm, cmdb_read_servers, cmdb_read_subnetworks, \
+    cmdb_create_subnetworks, cmdb_read_clusters, cmdb_create_clusters
+import json
+
+header = header_mercator()
+
+#print(header)
+
+# result = cmdb_read_clusters(8,header)
+#
+# print(result)
+
+data = read_vms()
+#
+# print(data)
+#
+result = cmdb_create_vms(data,header)
+
